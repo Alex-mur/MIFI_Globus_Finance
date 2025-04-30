1db4a3465afbd4bf26cb72ef338a0e9f21f7c30f
Andrew Kiryukhin <andrewkir2001@mail.ru>
2025-04-30 23:12:20 +0300

    feat(issue-10)

--- src/main/java/it/globus/finance/model/repo/TransactionRepo.java
+++ src/main/java/it/globus/finance/model/repo/TransactionRepo.java
@@ -3,5 +3,8 @@ package it.globus.finance.model.repo;
 import it.globus.finance.model.entity.Transaction;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TransactionRepo extends CrudRepository<Transaction, Long> {
+    List<Transaction> findByUser_Id(Long userId);
 }
